6bc5d35fb0704b3772ca4b42ea5150a1f1e33f1a
TNT <zzzTNTzzz@live.com>
2015-02-22 16:40:36 -0800

    High end currency
    this is how high end currency is put into sql trader

---
+++ Gem2item.sql
@@ -0,0 +1,15 @@
+-- ----------------------------
+-- Gem items
+-- ----------------------------
+INSERT INTO `traders_data` (`item`, `qty`, `buy`, `sell`, `tid`, `afile`) VALUES ('["MAAWS",3]',10,'[1,"ItemTopaz",1]','[5,"ItemGoldBar10oz",1]',526,'trade_weapons_old');
+INSERT INTO `traders_data` (`item`, `qty`, `buy`, `sell`, `tid`, `afile`) VALUES ('["MAAWS_HEAT",1]',10,'[1,"ItemObsidian",1]','[5,"ItemGoldBar10oz",1]',527,'trade_item_old');
+
+INSERT INTO `traders_data` (`item`, `qty`, `buy`, `sell`, `tid`, `afile`) VALUES ('["RPG7V",3]',10,'[1,"ItemTopaz",1]','[5,"ItemGoldBar10oz",1]',526,'trade_weapons_old');
+INSERT INTO `traders_data` (`item`, `qty`, `buy`, `sell`, `tid`, `afile`) VALUES ('["PG7V",1]',10,'[1,"ItemObsidian",1]','[5,"ItemGoldBar10oz",1]',527,'trade_item_old');
+INSERT INTO `traders_data` (`item`, `qty`, `buy`, `sell`, `tid`, `afile`) VALUES ('["PG7VR",1]',10,'[1,"ItemEmerald",1]','[5,"ItemGoldBar10oz",1]',527,'trade_item_old');
+INSERT INTO `traders_data` (`item`, `qty`, `buy`, `sell`, `tid`, `afile`) VALUES ('["PG7VL",1]',10,'[1,"ItemSapphire",1]','[5,"ItemGoldBar10oz",1]',527,'trade_item_old');
+
+INSERT INTO `traders_data` (`item`, `qty`, `buy`, `sell`, `tid`, `afile`) VALUES ('["BAF_AS50_scoped_Large",3]',10,'[1,"ItemRuby",1]','[5,"ItemGoldBar10oz",1]',526,'trade_weapons_old');
+INSERT INTO `traders_data` (`item`, `qty`, `buy`, `sell`, `tid`, `afile`) VALUES ('["5Rnd_127x99_as50",1]',10,'[1,"ItemCitrine",1]','[5,"ItemGoldBar10oz",1]',527,'trade_item_old');
+
+INSERT INTO `traders_data` (`item`, `qty`, `buy`, `sell`, `tid`, `afile`) VALUES ('["PipeBomb",1]',10,'[1,"ItemSapphire",1]','[5,"ItemGoldBar10oz",1]',527,'trade_item_old');
